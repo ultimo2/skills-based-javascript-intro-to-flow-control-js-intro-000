9ecbaeb8711cbce3b523bd163a9e714f12447d33
Scott Hale <ultimo2@msn.com>
2019-06-26 04:25:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -33,7 +33,25 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case (age >= 13 && age <= 19):
+    case 13:
+      return 'You are a teenager';
+      break;
+    case 14:
+      return 'You are a teenager';
+      break;
+    case 15:
+      return 'You are a teenager';
+      break;
+    case 16:
+      return 'You are a teenager';
+      break;
+    case 17:
+      return 'You are a teenager';
+      break;
+    case 18:
+      return 'You are a teenager';
+      break;
+    case 19:
       return 'You are a teenager';
       break;
     default:
